161da34ddad71b429fabcc589f03fe04bd911858
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-03-23 19:53:48 +0530

    minor changes

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -8,7 +8,9 @@ async function Header() {
   const userSession = await getUserSession();
   return (
     <header className="sticky top-0 mb-8 flex flex-col bg-white">
-      {userSession && <HeaderAuth name={userSession.user.name} />}
+      {userSession?.user.name ? (
+        <HeaderAuth name={userSession.user.name} />
+      ) : null}
       <div className="flex items-center justify-between px-8 py-4">
         {/* logo */}
         <div className="text-3xl font-bold uppercase">Ecommerce</div>
